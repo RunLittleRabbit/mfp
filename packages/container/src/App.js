488ef3aa5d8458b7d64a870b46d89392e06584b8
maximzonaris <zonarismaxim@gmail.com>
2024-01-25 20:01:41 +0100

    updated text in index.html

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 const App = () => {
   return <div>
-    <h1>App Container</h1>
+    <h1>App Container!</h1>
     <hr/>
     <MarketingApp />;
   </div>
